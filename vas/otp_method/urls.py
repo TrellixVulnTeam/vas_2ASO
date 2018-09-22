caa199a35bb6967ebc679260fe09463392ebcd8b
mehdi31075 <mehdi031075@yahoo.com>
2018-09-22 11:34:37 +0330

    pushotp & chargeotp created successfully

--- vas/otp_method/urls.py
+++ vas/otp_method/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('/data/', views.data),
+    path('/pushotp/', views.push_otp),
+    path('/chargeotp/', views.charge_otp),
     re_path('', views.detail),
-
 ]
--- vas/otp_method/views.py
+++ vas/otp_method/views.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Otp
+import requests
 import textwrap
 
 
@@ -16,7 +19,6 @@ def detail(request):
 
 
 def data(request):
-
     otps = Otp.objects.all()
     otp_ids = list()
 
@@ -28,3 +30,62 @@ def data(request):
     return HttpResponse(response_html)
 
 
+def push_otp(request):
+    xml = '''<?xml version="1.0" encoding="UTF-8"?>
+    <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:tem="http://tempuri.org/">
+<soap:Header/>
+<soap:Body>
+<tem:XmsRequest>
+<!--Optional:-->
+<tem:requestData><![CDATA[<xmsrequest>
+<userid>59344</userid>
+<password>Gx6yJrRk!T0p</password>
+<action>PushOtp</action>
+<body>
+<serviceid>680</serviceid>
+<recipient mobile="PHONE_NUMBER" originator="405002" cost="COST_NUMBER"></recipient>
+</body>
+</xmsrequest>]]></tem:requestData>
+</tem:XmsRequest>
+</soap:Body>
+</soap:Envelope>'''
+
+    headers = {"Content-Type": "text/xml"}
+
+    param1 = request.GET.get('mobile')
+    param2 = request.GET.get('cost')
+
+    return HttpResponse(requests.post('http://ws.smartsms.ir/sms.asmx',
+                                      data=xml.replace('PHONE_NUMBER', param1).replace('COST_NUMBER', param2),
+                                      headers=headers))
+
+
+def charge_otp(request):
+    xml = '''<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:tem="http://tempuri.org/">
+<soap:Header/>
+<soap:Body>
+<tem:XmsRequest>
+<!--Optional:-->
+<tem:requestData><![CDATA[<xmsrequest>
+<userid>59344</userid>
+<password>Gx6yJrRk!T0p</password>
+<action>chargeotp</action>
+<body>
+<serviceid>680</serviceid>
+<recipient mobile="PHONE_NUMBER" originator="405002" pin="PIN_CODE">OTP_ID</recipient>
+</body>
+</xmsrequest>]]></tem:requestData>
+</tem:XmsRequest>
+</soap:Body>
+</soap:Envelope>
+'''
+
+    headers = {"Content-Type": "text/xml"}
+
+    param1 = request.GET.get('mobile')
+    param2 = request.GET.get('pin')
+    param3 = request.GET.get('otpid')
+
+    return HttpResponse(requests.post('http://ws.smartsms.ir/sms.asmx',
+                                      data=xml.replace('PHONE_NUMBER', param1).replace('PIN_CODE', param2).replace(
+                                          'OTP_ID', param3), headers=headers))
